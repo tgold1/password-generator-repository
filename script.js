685cc081e5ab262475e3989f09ff11508bd344ee
Tim Gold <timothygold@comcast.net>
2023-03-23 09:52:32 -0400

    added script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var numberofcharacters = 0
 var answer = numberofcharacters
-var specialcharacters = 0
+var specialcharacters = ["#", "@", "!", "$", "%", "^", "&", "*", "_", "-","<", ">", "?", ".", "+"]
 var Uppercaseletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var Lowercaseletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
