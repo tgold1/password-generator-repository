0e220761d916f8a28fbcb6f35aeeed3f87190e25
Tim Gold <timothygold@comcast.net>
2023-03-23 17:18:13 -0400

    added script.js

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 // Assignment Code
-var numberofcharacters = 0
-var answer = numberofcharacters
 var specialcharacters = ["#", "@", "!", "$", "%", "^", "&", "*", "_", "-","<", ">", "?", ".", "+"]
 var Uppercaseletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var Lowercaseletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 function generatePassword () { 
-  window.prompt("How many characters would you like your password to be?")
+  var numberofcharacters = window.prompt("How many characters would you like your password to be?")
+  if (numberofcharacters < 8 == true) window.alert("Character length must be between 8-128 characters.") 
+  else if(numberofcharacters >128 == true) window.alert ("Character length must be between 8-128 characters.")
+  
+  else
+ 
+
   window.confirm("Would you like to use special characters?");
   window.confirm("Would you like to use Uppercase letters?" );
   window.confirm("Would you like to use Lowercase letters?");
   window.confirm("Would you like to use Numbers?");
-//if (numberofcharacters < 8) {
-//  prompt = "Password must be a minimum of 8 characters";
-//}
-//if (numberofcharacters > 128) {
-  prompt = "Password must be a maximum of 128 characters";
-//}
+
+  return
+
+if (specialcharacters == true) {
+  specialcharacters.concat (specialcharacters)
+
+}
+
+if (Uppercaseletters == true) {
+  Uppercaseletters.concat (Uppercaseletters)
+}
+
+if (Lowercaseletters == true) {
+  Lowercaseletters.concat (Lowercaseletters)
+}
+
+if (Numbers == true)  {
+  Numbers.concat (Numbers)
+}
+
+var index = math.floor(math.random () * password.length)
+
+for (let i = 0; i< password.length; i++) {
+
+}
+
+
  
   return ;
 }
